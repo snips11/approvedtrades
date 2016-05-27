9683ae58b5a68dfdd37bc4c3f67f3d060a426c6a
snips11 <james@parsonsgreenmedia.com>
2016-05-27 14:02:18 +0100

    27/05/16

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('trades', function () {
+    return view('Trades.trades');
+});
+
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
